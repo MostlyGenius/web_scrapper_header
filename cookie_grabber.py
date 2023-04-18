18e697a4ee60084ae9d802452a28171628350a2c
MostlyGenius <90004142+MostlyGenius@users.noreply.github.com>
2023-04-18 10:03:03 -0700

    Update cookie_grabber.py

--- cookie_grabber.py
+++ cookie_grabber.py
@@ -1,1 +1,22 @@
 
+import requests
+
+url = 'https://example.com'  # Replace with the desired URL
+
+# Create a session to manage cookies
+session = requests.Session()
+
+# Make a request to the website
+response = session.get(url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Get all cookies from the session
+    cookies = session.cookies.get_dict()
+
+    # Print the cookies
+    print("Cookies obtained:")
+    for key, value in cookies.items():
+        print(f"{key}: {value}")
+else:
+    print("Failed to fetch the webpage.")
